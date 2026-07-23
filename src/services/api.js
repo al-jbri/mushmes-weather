38fb5048e37d267836b5c9c6f0d0efb6da3e17ec
Ahmed al-jbri <aljbri.dev@gmail.com>
2026-07-23 14:44:04 +0300

    feat: adding error handling to the api.js functions

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+async function getGeocodefromCity(city) {
+  try {
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`;
+    const res = await fetch(url);
+    return await res.json();
+  } catch {
+    return null;
+  }
+}
+
+async function getWeather(latitude, longitude) {
+  try {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+    const res = await fetch(url);
+    return await res.json();
+  } catch {
+    return null;
+  }
+}
+
+export { getGeocodefromCity, getWeather };
