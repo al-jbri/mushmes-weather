3ed3a838a7df524f789e6434594272afe896d0c1
Ahmed al-jbri <aljbri.dev@gmail.com>
2026-07-23 14:46:00 +0300

    feat: finish the main citySelector.jsx logic with error handling

--- src/components/CitySelector.js
+++
@@ -1,3 +0,0 @@
-export default function CitySelector() {
-  return <div>"no"</div>;
-}
---
+++ src/components/CitySelector.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { getGeocodefromCity } from "@/services/api.js";
+import toast from "react-hot-toast";
+
+export default function CitySelector({ setGeocode }) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const cityName = e.target.elements.cityInput.value;
+
+    // check input
+    if (!cityName.trim()) {
+      toast.error("Please enter a city name");
+      return;
+    }
+
+    // fetch and check data
+    const data = await getGeocodefromCity(cityName);
+    !data && toast.error("Failure to contact the server");
+    !data.results && toast.error("City not found, try another name!");
+
+    // save data
+
+    const geocodeData = {
+      name: data.results[0].name,
+      latitude: data.results[0].latitude,
+      longitude: data.results[0].longitude,
+    };
+
+    window.localStorage.setItem("geocode", JSON.stringify(geocodeData));
+    setGeocode(localStorage.getItem("geocode"));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="cityInput">City Name</label>
+      <input
+        type="text"
+        name="cityInput"
+        id="cityInput"
+        placeholder="search for city name..."
+      />
+    </form>
+  );
+}
